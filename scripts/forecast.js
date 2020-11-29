bf22078a0d75e7a29f4061c7ec7377011824aee6
Patryk Czarnecki <cph-pc97@cphbusiness.dk>
2020-11-29 02:20:41 +0100

    Minor changes

---
+++ scripts/forecast.js
@@ -0,0 +1,28 @@
+// Maximum 50 requests to the API per day.
+const key = 'GG8HjSofENPSYJgHghGWLUR0Ckm86mA4';
+
+// Get weather information
+const getWeather = async (id) => {
+  
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+// Get city information
+const getCity = async (city) => {
+
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
